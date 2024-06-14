bc83324fe35caeeaf43be8d38ff1d7c270339104
Toma Alexandru <alextoma1704@gmail.com>
2024-06-14 18:03:02 +0300

    Add comments functionality added

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,10 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn, token } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
   apiVersion,
+  dataset,
+  projectId,
   useCdn,
   token,
-  perspective: 'published',
 })
