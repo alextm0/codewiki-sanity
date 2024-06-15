55e2a8616ced849d386a75ce0e9cea4e382a80f7
Toma Alexandru <alextoma1704@gmail.com>
2024-06-15 18:40:26 +0300

    Added README.md

--- app/(client)/tag/[slug]/page.tsx
+++ app/(client)/tag/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/app/components/Header";
-import PostComponent from "@/app/components/Old_PostComponent";
+import PostComponent from "@/app/components/PostComponent";
 import { Post } from "@/app/utils/interface";
 import { client } from "@/sanity/lib/client";
 import React from "react";
