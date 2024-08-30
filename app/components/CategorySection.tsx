ebe99d1b9e1d6f8ff682d55c1827bd35987512b2
Toma Alexandru <alextoma1704@gmail.com>
2024-08-30 17:07:41 +0300

    Changed the layout of the roadmaps

--- app/components/CategorySection.tsx
+++ app/components/CategorySection.tsx
@@ -26,6 +26,9 @@ const CategorySection: React.FC<CategoryPageProps> = ({
   topics,
   description,
 }) => {
+  // Hooks should be declared at the top level of the component
+  const [hoverIndex, setHoverIndex] = useState<number | null>(null); 
+
   if (!name || !category || !topics) {
     return null;
   }
@@ -48,34 +51,30 @@ const CategorySection: React.FC<CategoryPageProps> = ({
             <div className="ml-10 col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8">
               {/* Improved line design */}
               <div className="sm:before:absolute sm:before:top-0 sm:before:bottom-0 sm:before:w-1 sm:before:-left-3 before:bg-gradient-to-b before:from-primary-200 before:to-primary-200 before:rounded-full  sm:before:border-violet-400 sm:before:shadow-lg sm:before:opacity-75">
-                {topics.map((topic) => {
-                  const [hover, setHover] = useState(false); // Manage hover state here
-
-                  return (
-                    <Link
-                      key={uuidv4()}
-                      href={`/posts/${slugify(topic.topicName)}`}
-                      className="block"
-                      onMouseEnter={() => setHover(true)}
-                      onMouseLeave={() => setHover(false)}
-                    >
-                      <div className="font-poppins flex flex-col p-6 mb-6 rounded-lg shadow-sm hover:shadow-lg hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 transition-transform duration-500 ease-in-out transform hover:-translate-y-1 hover:border-l-4 hover:border-primary-00">
-                        <div className="flex items-center">
-                          <div className="flex-shrink-0 w-4 h-4 mr-4 rounded-full bg-primary-200"></div>
-                          <div className="text-xl font-semibold tracking-wide text-gray-800 dark:text-gray-300">
-                            {topic.topicName}
-                          </div>
-                        </div>
-                        <div className="mt-2">
-                          <Rating stars={topic.stars} onHover={hover} onBlogPost={true} />
-                        </div>
-                        <div className="mt-3 text-gray-500 font-inter">
-                          {topic.details}
+                {topics.map((topic, index) => (
+                  <Link
+                    key={uuidv4()}
+                    href={`/posts/${slugify(topic.topicName)}`}
+                    className="block"
+                    onMouseEnter={() => setHoverIndex(index)}
+                    onMouseLeave={() => setHoverIndex(null)}
+                  >
+                    <div className="font-poppins flex flex-col p-6 mb-6 rounded-lg shadow-sm hover:shadow-lg hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 transition-transform duration-500 ease-in-out transform hover:-translate-y-1 hover:border-l-4 hover:border-primary-00">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 w-4 h-4 mr-4 rounded-full bg-primary-200"></div>
+                        <div className="text-xl font-semibold tracking-wide text-gray-800 dark:text-gray-300">
+                          {topic.topicName}
                         </div>
                       </div>
-                    </Link>
-                  );
-                })}
+                      <div className="mt-2">
+                        <Rating stars={topic.stars} onHover={hoverIndex === index} onBlogPost={true} />
+                      </div>
+                      <div className="mt-3 text-gray-500 font-inter">
+                        {topic.details}
+                      </div>
+                    </div>
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
