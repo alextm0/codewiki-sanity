64fcf91de09e8a3ec3dc51d259a6364de2e4c6c5
Toma Alexandru <alextoma1704@gmail.com>
2024-09-02 18:05:13 +0300

    Added Google Tag Manager scripts

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -40,6 +40,18 @@ export default function RootLayout({
     <ViewTransitions>
       <html lang="en" className="overflow-x-hidden bg-background-50 h-full">
         <head>
+        <Script
+            id="gtm-head"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NCFGTGC');`,
+            }}
+          ></Script>
+
           {/* Preload key fonts */}
           <link
             rel="preload"
@@ -139,6 +151,15 @@ export default function RootLayout({
           ></Script>
         </head>
         <body className="h-full flex flex-col">
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NCFGTGC"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+
           <CustomThemeProvider>
             <Navbar />
             <main className="flex-grow pb-20">
